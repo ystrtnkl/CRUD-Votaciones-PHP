71e354899158a0b563674eeed6c7342b5c5b3399
ystrtnkl <freewillfooljoy@gmail.com>
2025-10-03 17:14:05 +0200

    a

--- varwwwhtml/proyecto/views/v_verEncuesta.php
+++ varwwwhtml/proyecto/views/v_verEncuesta.php
@@ -3,9 +3,17 @@
 <body>
 <?php require(DIR_VIEWS . "template/vt_cabecera.php") ?>
 <main>
-    <h2>ver encuesta</h2>
+    <h2>Ver encuesta</h2>
     <p>requiere valor en url</p>
     <p>requiere tener cuenta y permisos a esa encuesta</p>
+    <p>cambia dependiendo si eres el duegno</p>
+    <h3>Titulo de la encuesta: </h3>
+    <h3>Contenido: </h3>
+    <p>aaaa</p>
+    <h3>Escribe tu respuesta: </h3>
+    <form action="">
+
+    </form>
 
 
 

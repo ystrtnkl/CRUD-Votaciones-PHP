3600a7659fe13de6ca6eccd55b9b893c06609e61
ystrtnkl <freewillfooljoy@gmail.com>
2025-10-08 20:43:57 +0200

    a

--- varwwwhtml/proyecto/views/template/vt_footer.php
+++ varwwwhtml/proyecto/views/template/vt_footer.php
@@ -1,9 +1,7 @@
 <?php
 
 ?>
-<div class="container">
     <footer >
                     <p>Un CRUD básico para registrar usuarios que podrán crear y participar en encuestas sencillas. <br> Instalar dependencias de composer con: cd varwwwhtml/proyecto/ ; composer install ; composer update ;<br>Se puede iniciar con: sh .docker/auto.sh</p>
 
     </footer>
-</div>
